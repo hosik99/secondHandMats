969f23392c26074f71bf31716e6a4cfb39cecdae
hosik01 <kimicetea777@gmail.com>
2022-10-10 16:54:05 +0900

    aop를 사용하여 안읽은 메세지 갯수 표시

--- src/main/java/com/mat/aop/MyPageAop.java
+++ src/main/java/com/mat/aop/MyPageAop.java
@@ -1,12 +1,17 @@
 package com.mat.aop;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.mat.security.SecurityUser;
 import com.mat.service.MesaageService;
@@ -29,6 +34,10 @@ public class MyPageAop {
 	
 	@AfterReturning("execution(* com.mat.controller.MyPageController.*(..))") 
     public void before(){
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+		SecurityUser principal = (SecurityUser)SecurityContextHolder.getContext().getAuthentication().getPrincipal(); 
+		Long unReadMsgCnt = mesaageService.unReadMsgCnt(principal.getUsername());
+		request.setAttribute("messageCnt", unReadMsgCnt);
     }
 	
 }
